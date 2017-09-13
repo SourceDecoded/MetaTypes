40142919b101128ac7c863e32d77bb891c7485a2
Jared Barnes <jared-barnes@leavitt.com>
2017-09-13 11:45:15 -0600

    Trying to fix packaging bug.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    node: {
-        // This is for a bug https://github.com/webpack-contrib/css-loader/issues/447
-        // Without it, it won't compile.
-        fs: "empty"
-    },
+    target: "node",
     entry: './lib/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
