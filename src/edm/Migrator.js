c6c07c84d9026c6b868f0da1aa06e53ba351cd3c
Jared Barnes <jared-barnes@leavitt.com>
2017-10-19 09:58:34 -0600

    Testing.

--- src/edm/Migrator.js
+++ src/edm/Migrator.js
@@ -2,98 +2,150 @@ import Validator from "./Validator";
 
 const resolvedPromise = Promise.resolve;
 
-export default class Migrator {
-    constructor() {
-        this.validator = new Validator();
-        this.name = "Edm Migrator";
+const isEqualTo = (left, right) => {
+    let leftKeys = Object.keys(left);
+    let rightKeys = Object.keys(right);
+
+    if (leftKeys.length !== rightKeys.length) {
+        return false;
     }
 
-    _doesTableHavePrimaryKey(table) {
+    return leftKeys.every((key) => {
+        left[key] === right[key];
+    });
+}
+
+export default class Migrator {
+    constructor(edm) {
+        this.validator = new Validator();
+        this.name = "Edm Migrator";
+        this.edm = edm;
+    }
+
+    _doesTableHavePrimaryKey(tableName) {
+        let table = this._getTable(tableName);
+
         return table.columns.find((column) => {
             return column.isPrimaryKey;
         }) != null;
     }
 
-    _getColumn(columns, name) {
+    _getColumn(tableName, columnName) {
+        this._throwIfTableDoesNotExist(tableName);
+
+        let columns = this._getTable(tableName).columns;
+
         return columns.find((column) => {
-            return column.name === name;
+            return column.name === columnName;
         });
     }
 
-    _getDecorator(table, decoratorName) {
+    _getDecorator(tableName, decoratorName) {
+        this._throwIfTableDoesNotExist(tableName);
+        let table = this._getTable(tableName);
+
         return table.decorators.find((decorator) => {
             return decorator.name === decoratorName;
         })
     }
 
-    _getTable(tables, name) {
+    _getTable(name) {
+        let tables = this.edm.tables;
+
         return tables.find((table) => {
             return table.name === name;
         });
     }
 
-    _hasDecorator(table, decoratorName) {
-        return this._getDecorator(table, decoratorName) != null;
+    _hasDecorator(tableName, decoratorName) {
+        return this._getDecorator(tableName, decoratorName) != null;
     }
 
     _isEmptyString(string) {
         return string == null || typeof string !== "string" || string === "";
     }
 
-    _throwIfColumnExist(edm, columns, columnName) {
-        let column = this._getColumn(columns, columnName);
+    _setColumn(tableName, columnName, column) {
+        this._throwIfColumnDoesNotExist();
+        let columns = this._getTable(table).column;
+
+        let index = columns.findIndex((column) => {
+            column.name === columnName;
+        });
+
+        columns.splice(index, 1, column);
+    }
+
+    _setDecorator(decorators, decoratorName, decorator) {
+        let index = decorators.findIndex((decorator) => {
+            decorator.name === decoratorName;
+        });
+
+        decorators.splice(index, 1, decorator);
+    }
+
+    _replaceOneToOneRelationship(oldRelationship, newRelationship) {
+
+    }
+
+    _replaceOneToManyRelationship(oldRelationship, newRelationship) {
+
+    }
+
+    _throwIfColumnExist(tableName, columnName) {
+        let column = this._getColumn(tableName, columnName);
 
         if (column != null) {
             throw new Error(`'${columnName}' column already exists.`);
         }
     }
 
-    _throwIfColumnDoesNotExist(edm, columns, columnName) {
-        let column = this._getColumn(columns, columnName);
+    _throwIfColumnDoesNotExist(tableName, columnName) {
+        let column = this._getColumn(tableName, columnName);
 
         if (column == null) {
             throw new Error(`'${columnName}' column doesn't exists.`);
         }
     }
 
-    _throwIfTableExist(edm, name) {
-        let table = this._getTable(edm.tables, name);
+    _throwIfTableExist(name) {
+        let table = this._getTable(name);
 
         if (table != null) {
             throw new Error(`'${name}' table already exists.`);
         }
     }
 
-    _throwIfTableDoesNotExist(edm, name) {
-        let table = this._getTable(edm.tables, name);
+    _throwIfTableDoesNotExist(name) {
+        let table = this._getTable(name);
 
         if (table == null) {
             throw new Error(`'${name}' table doesn't exists.`);
         }
     }
 
-    addColumnAsync(edm, options = {}) {
-        let table = this._getTable(edm.tables, options.tableName);
-        this._throwIfColumnExist(edm, table.columns, options.column.name);
+    addColumnAsync(options = {}) {
+        let table = this._getTable(options.tableName);
 
+        this._throwIfColumnExist(options.tableName, options.column.name);
         this.validator.validateColumn(options.column);
 
         if (table.columns.length === 0 && !options.column.isPrimaryKey) {
             throw new Error("First column to a table needs to be a primary key.");
         }
 
-        if (this._doesTableHavePrimaryKey(table) && options.column.isPrimaryKey) {
-            throw new Error(`The ${options.table} can only have one primary key.`);
+        if (this._doesTableHavePrimaryKey(options.tableName) && options.column.isPrimaryKey) {
+            throw new Error(`The ${options.tableName} can only have one primary key.`);
         }
 
-        table.columns.push(options.column);
+        table.columns.push(Object.assign({}, options.column));
 
         return resolvedPromise;
     }
 
-    addDecoratorAsync(edm, options = {}) {
-        let table = this._getTable(edm.tables, options.tableName);
-        this._throwIfTableDoesNotExist(edm, options.tableName);
+    addDecoratorAsync(options = {}) {
+        this._throwIfTableDoesNotExist(options.tableName);
+        let table = this._getTable(options.tableName);
 
         this.validator.validateDecorator(options.decorator);
 
@@ -101,26 +153,26 @@ export default class Migrator {
             throw new Error(`The '${options.decorator.name}' decorator already exists on the '${options.tableName}' table.`);
         }
 
-        table.decorators.push(options.decorator);
+        table.decorators.push(Object.assign({}, options.decorator));
 
         return resolvedPromise;
     }
 
-    addOneToOnRelationship(edm, options) {
+    addOneToOnRelationship(options) {
         this.validator.validateOneToOneRelationship(options.relationship);
-        edm.relationships.oneToOne.push(options.relationship);
+        this.edm.relationships.oneToOne.push(Object.assign({}, options.relationship));
     }
 
-    addOneToManyRelationship(edm, options) {
+    addOneToManyRelationship(options) {
         this.validator.validateOneToManyRelationship(options.relationship);
-        edm.relationships.oneToMany.push(options.relationship);
+        this.edm.relationships.oneToMany.push(Object.assign({}, options.relationship));
     }
 
-    addTableAsync(edm, options = {}) {
+    addTableAsync(options = {}) {
         this.validator.validateTableDescriptors(options);
-        this._throwIfTableExist(edm, options.name);
+        this._throwIfTableExist(options.name);
 
-        edm.tables.push({
+        this.edm.tables.push({
             name: options.name,
             label: options.label,
             pluralLabel: options.pluralLabel,
@@ -131,12 +183,12 @@ export default class Migrator {
         return resolvedPromise;
     }
 
-    removeColumnAsync(edm, options = {}) {
-        let table = this._getTable(edm.tables, options.tableName);
-        let column = this._getColumn(table.columns, options.column.name);
+    removeColumnAsync(options = {}) {
+        this._throwIfTableDoesNotExist(options.tableName);
+        this._throwIfColumnDoesNotExist(options.tableName, options.column.name);
 
-        this._throwIfTableDoesNotExist(edm, options.tableName);
-        this._throwIfColumnDoesNotExist(edm, table.columns, options.column.name);
+        let table = this._getTable(options.tableName);
+        let column = this._getColumn(options.tableName, options.column.name);
 
         if (column.isPrimaryKey && table.columns.length > 1) {
             throw new Error("Cannot remove the primary key.");
@@ -145,11 +197,11 @@ export default class Migrator {
         return resolvedPromise;
     }
 
-    removeDecoratorAsync(edm, options = {}) {
-        let table = this._getTable(edm.tables, options.tableName);
-        this._throwIfTableDoesNotExist(edm, options.tableName);
+    removeDecoratorAsync(options = {}) {
+        this._throwIfTableDoesNotExist(options.tableName);
+        let table = this._getTable(options.tableName);
 
-        let decorator = this._getDecorator(table, options.decorator.name);
+        let decorator = this._getDecorator(options.tableName, options.decorator.name);
 
         if (decorator == null) {
             throw new Error(`The ${options.tableName} doesn't have the ${options.decorator.name} to update.`);
@@ -164,23 +216,24 @@ export default class Migrator {
         return resolvedPromise;
     }
 
-    removeTableAsync(edm, options = {}) {
-        this._throwIfTableDoesNotExist(edm, options.name);
+    removeTableAsync(options = {}) {
+        this._throwIfTableDoesNotExist(options.name);
 
-        let index = edm.tables.findIndex((table) => {
+        let index = this.edm.tables.findIndex((table) => {
             return table.name === options.name;
         });
 
-        edm.tables.splice(index, 1);
+        this.edm.tables.splice(index, 1);
 
         return resolvedPromise;
     }
 
-    updateColumnAsync(edm, options = {}) {
-        let table = this._getTable(edm.tables, options.tableName);
-        this._throwIfTableDoesNotExist(edm, table.columns, options.column.name);
+    updateColumnAsync(options = {}) {
+        let table = this._getTable(options.tableName);
+        this._throwIfTableDoesNotExist(options.tableName);
+        this._throwIfColumnDoesNotExist(options.tableName, options.column.name);
 
-        let column = this._getColumn(table.columns, options.column.name);
+        let column = this._getColumn(options.tableName, options.column.name);
         let updatedColumn = Object.assign({}, column, options.column);
 
         this.validator.validateColumn(updatedColumn);
@@ -194,13 +247,13 @@ export default class Migrator {
         return resolvedPromise;
     }
 
-    updateDecoratorAsync(edm, options = {}) {
-        let table = this._getTable(edm.tables, options.tableName);
-        this._throwIfTableDoesNotExist(edm, options.tableName);
+    updateDecoratorAsync(options = {}) {
+        this._throwIfTableDoesNotExist(options.tableName);
 
+        let table = this._getTable(options.tableName);
         this.validator.validateDecorator(options.decorator);
 
-        let decorator = this._getDecorator(table, options.decorator.name);
+        let decorator = this._getDecorator(options.tableName, options.decorator.name);
 
         if (decorator == null) {
             throw new Error(`The ${options.tableName} doesn't have the ${options.decorator.name} to update.`);
@@ -211,10 +264,10 @@ export default class Migrator {
         return resolvedPromise;
     }
 
-    updateTableAsync(edm, options = {}) {
-        this._throwIfTableDoesNotExist(edm, options.name);
+    updateTableAsync(options = {}) {
+        this._throwIfTableDoesNotExist(options.name);
 
-        let table = this._getTable(edm.tables, options.name);
+        let table = this._getTable(options.name);
         this.validator.validateTableDescriptors(Object.assign({}, table, options));
 
         // We want to make sure that the developer doesn't change the columns and decorators here.
